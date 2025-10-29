a20a564eb8026860552d9d6751b79eef9d999c6b
Gio Viera <viera.giotheodore@Student.auf.edu.ph>
2025-10-29 10:02:52 +0800

    Fix semicolons to pass ESLint

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function greet() {
-  console.log("Hello from the ugliest UI site!") // <- no semicolon (for lint test)
+  console.log("Hello from the ugliest UI site!"); // <- no semicolon (for lint test)
 }
 
 greet()
